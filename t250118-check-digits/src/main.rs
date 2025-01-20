4cec2af2bf4f97147cbb9ca79a20a636630453f0
lispking <lispking@qq.com>
2025-01-20 09:41:25 +0800

    add t250118-check-digits v3

--- t250118-check-digits/src/main.rs
+++ t250118-check-digits/src/main.rs
@@ -11,7 +11,15 @@ fn is_all_digits_v2(s: &str) -> bool {
     s.chars().all(|char| char.is_digit(10))
 }
 
+fn is_all_digits_v3(s: &str) -> bool {
+    s.chars()
+        .map(|c| c.is_ascii_digit() as u8)
+        .fold(1, |acc, is_digit| acc & is_digit)
+        == 1
+}
+
 fn main() {
     println!("is_all_digits: {}", is_all_digits("123456"));
     println!("is_all_digits_v2: {}", is_all_digits_v2("123456"));
+    println!("is_all_digits_v2: {}", is_all_digits_v3("123456"));
 }
